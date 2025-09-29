ce31b1486ac6b6260472be4ad49fa747ceb43f5e
ninhgiang225 <ngnguy26@colby.edu>
2025-09-29 17:38:52 -0400

    overrid and modify equals so that it checked all the fields in item object

--- src/CartItem.java
+++ src/CartItem.java
@@ -25,8 +25,15 @@ public class CartItem {
         this.quantity = quantity;
     }
 
-    public boolean equals(CartItem item) {
-        return this.itemName.equals(item.getName());
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        CartItem other = (CartItem) obj;
+        return itemName.equals(other.itemName) &&               // check name
+               Double.compare(price, other.price) == 0 &&       // check price
+               quantity == other.quantity;                      // check quantity
     }
 
     public double getTotalPrice() {
