e1abf8cdfb31753f8476fdde46b8f15342a133e6
roseagyapong08 <rfagyapong1@gmail.com>
2025-09-29 13:46:03 -0400

    feat: add Address class for representing addresses

---
+++ src/Address.java
@@ -0,0 +1,17 @@
+public class Address {
+    private String line1;
+    private String line2;
+    private String city;
+    private String state;
+    private String zip;
+    private String country;
+
+    public Address(String line1, String line2, String city, String state, String zip, String country) {
+        this.line1 = line1;
+        this.line2 = line2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.country = country;
+    }
+}
