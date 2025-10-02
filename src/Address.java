bf3384fe5b24d00cb3010847072f976de937b8e7
francis-ohara <franciskohara@gmail.com>
2025-10-02 12:55:38 -0400

    feat: add toString() method for Address class

--- src/Address.java
+++ src/Address.java
@@ -14,4 +14,9 @@ public class Address {
         this.zip = zip;
         this.country = country;
     }
+
+    @Override
+    public String toString() {
+        return line1 + ", " + line2 + ", " + city + ", " + state + ", " + zip + ", " + country;
+    }
 }
